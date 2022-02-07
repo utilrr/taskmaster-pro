e42743865b01747c83545a61f25028f772908fc7
Mark J. Healy <gmarkghealy@gmail.com>
2022-02-07 13:41:19 -0500

    added drag & drop script for columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -149,8 +149,41 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
+// make columns sortable
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
 
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
 
+      console.log(text, date);
+    });
+  }
+});
 
 
 // modal was triggered
